d583a6382557fa711555362e6c4a102dbe46fe74
yo123yoyo <boy.catalin12@gmail.com>
2024-09-28 03:55:29 +0300

    Update update_el.sh

--- update_el.sh
+++ update_el.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd /root/conf/dock/elixir/
+cd /home/config/docker/docker-compose/go/elixir/
 
 docker kill elixir &>/dev/null
 docker rm -f elixir &>/dev/null
 docker pull elixirprotocol/validator:v3 --platform linux/amd64
 
-docker run --env-file /root/conf/dock/elixir/.env --name elixir --platform linux/amd64 --restart always -p 17690:17690 elixirprotocol/validator:v3
+docker run --env-file /home/config/docker/docker-compose/go/elixir/.env --name elixir --platform linux/amd64 --restart always -p 17690:17690 elixirprotocol/validator:v3
