5e529423747807275b27d57fb38f40a06a44078b
yo123yoyo <boy.catalin12@gmail.com>
2024-10-31 23:11:01 +0200

    Update update_el.sh

--- update_el.sh
+++ update_el.sh
@@ -4,6 +4,7 @@ cd /home/config/docker/docker-compose/go/elixir/
 
 docker kill elixir &>/dev/null
 docker rm -f elixir &>/dev/null
-docker pull elixirprotocol/validator:v3 --platform linux/amd64
+sed -i 's/^ENV=testnet-3/ENV=prod/' /home/config/docker/docker-compose/go/elixir/.env
+docker pull elixirprotocol/validator --platform linux/amd64
 
-docker run --env-file /home/config/docker/docker-compose/go/elixir/.env --name elixir --platform linux/amd64 --restart always -p 17690:17690 elixirprotocol/validator:v3
+docker run --env-file /home/config/docker/docker-compose/go/elixir/.env --name elixir --platform linux/amd64 --restart always -p 17690:17690 elixirprotocol/validator
