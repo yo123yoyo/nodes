a97162652fd5422e60dbbf4b9034f6e7c77ced25
yo123yoyo <boy.catalin12@gmail.com>
2024-12-14 00:23:51 +0200

    Update setup_linux.sh

--- setup_linux.sh
+++ setup_linux.sh
@@ -9,14 +9,14 @@ fi
 CLAIM_REWARD_ADDRESS=$1
 
 # 第一段命令：删除旧的cysic-verifier目录，创建新的目录，并下载必要的文件
-rm -rf ~/info
-cd ~
-mkdir info
-curl -L https://github.com/cysic-labs/phase2_libs/releases/download/v1.0.0/verifier_linux > ~/info/verifier
-curl -L https://github.com/cysic-labs/phase2_libs/releases/download/v1.0.0/libdarwin_verifier.so > ~/info/libdarwin_verifier.so
+rm -rf ~/cysic-verifier
+
+mkdir cysic-verifier
+curl -L https://github.com/cysic-labs/phase2_libs/releases/download/v1.0.0/verifier_linux > ~/cysic-verifier/verifier
+curl -L https://github.com/cysic-labs/phase2_libs/releases/download/v1.0.0/libdarwin_verifier.so > ~/cysic-verifier/libdarwin_verifier.so
 
 # 第二段命令：创建配置文件
-cat <<EOF > ~/info/config.yaml
+cat <<EOF > ~/cysic-verifier/config.yaml
 # Not Change
 chain:
   # Not Change
@@ -38,7 +38,7 @@ server:
 EOF
 
 # 第三段命令：设置执行权限并启动verifier
-cd ~/info/
-chmod +x ~/info/verifier
-echo "LD_LIBRARY_PATH=. CHAIN_ID=534352 ./verifier" > ~/info/start.sh
-chmod +x ~/info/start.sh
+cd ~/cysic-verifier/
+chmod +x ~/cysic-verifier/verifier
+echo "LD_LIBRARY_PATH=. CHAIN_ID=534352 ./verifier" > ~/cysic-verifier/start.sh
+chmod +x ~/cysic-verifier/start.sh
