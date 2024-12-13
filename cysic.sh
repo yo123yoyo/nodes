c28f926956e2fcfd6489d1e0b7903097357f9a3b
yo123yoyo <boy.catalin12@gmail.com>
2024-12-14 00:09:38 +0200

    Create cysic.sh

---
+++ cysic.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# 检查是否传入了参数
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <claim_reward_address>"
+    exit 1
+fi
+
+CLAIM_REWARD_ADDRESS=$1
+
+# 第一段命令：删除旧的cysic-verifier目录，创建新的目录，并下载必要的文件
+rm -rf ~/cysic-verifier
+cd ~
+mkdir info
+curl -L https://github.com/cysic-labs/phase2_libs/releases/download/v1.0.0/verifier_linux > ~/cysic-verifier/verifier
+curl -L https://github.com/cysic-labs/phase2_libs/releases/download/v1.0.0/libdarwin_verifier.so > ~/cysic-verifier/libdarwin_verifier.so
+
+# 第二段命令：创建配置文件
+cat <<EOF > ~/info/config.yaml
+# Not Change
+chain:
+  # Not Change
+  # endpoint: "node-pre.prover.xyz:80"
+  endpoint: "grpc-testnet.prover.xyz:80"
+  # Not Change
+  chain_id: "cysicmint_9001-1"
+  # Not Change
+  gas_coin: "CYS"
+  # Not Change
+  gas_price: 10
+  # Modify Here：! Your Address (EVM) submitted to claim rewards
+claim_reward_address: "$CLAIM_REWARD_ADDRESS"
+
+server:
+  # don't modify this
+  # cysic_endpoint: "https://api-pre.prover.xyz"
+  cysic_endpoint: "https://api-testnet.prover.xyz"
+EOF
+
+# 第三段命令：设置执行权限并启动verifier
+cd ~/info/
+chmod +x ~/info/verifier
+echo "LD_LIBRARY_PATH=. CHAIN_ID=534352 ./verifier" > ~/info/start.sh
+chmod +x ~/info/start.sh
