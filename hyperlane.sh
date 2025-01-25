495ce0da0b2a0332f8bae096baff0badddfa0279
yo123yoyo <boy.catalin12@gmail.com>
2025-01-25 20:14:40 +0200

    Update hyperlane.sh

--- hyperlane.sh
+++ hyperlane.sh
@@ -1,125 +1,193 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Цвета текста
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-NC='\033[0m' # Нет цвета (сброс цвета)
+channel_logo() {
+  echo -e '\033[0;31m'
+  echo -e '┌┐ ┌─┐┌─┐┌─┐┌┬┐┬┬ ┬  ┌─┐┬ ┬┌┐ ┬┬  '
+  echo -e '├┴┐│ ││ ┬├─┤ │ │└┬┘  └─┐└┬┘├┴┐││  '
+  echo -e '└─┘└─┘└─┘┴ ┴ ┴ ┴ ┴   └─┘ ┴ └─┘┴┴─┘'
+  echo -e '\e[0m'
+  echo -e "\n\nПодпишись на самый 4ekHyTbIu* канал в крипте @bogatiy_sybil [💸]"
+}
 
-# Проверка наличия curl и установка, если не установлен
-if ! command -v curl &> /dev/null; then
-    sudo apt update
-    sudo apt install curl -y
-fi
-sleep 1
+download_node() {
+  if [ -f "wallet_hyperlane.json" ]; then
+    echo "Нода уже существует. Выполнение остановлено. Удалите ноду если хотите установить заново"
+    exit 1
+  fi
 
-# Отображаем логотип
-curl -s https://raw.githubusercontent.com/noxuspace/cryptofortochka/main/logo_club.sh | bash
+  echo 'Начинаю установку ноды...'
 
-# Меню
-    echo -e "${YELLOW}Выберите действие:${NC}"
-    echo -e "${CYAN}1) Установка ноды${NC}"
-    echo -e "${CYAN}2) Обновление ноды${NC}"
-    echo -e "${CYAN}3) Просмотр логов${NC}"
-    echo -e "${CYAN}4) Удаление ноды${NC}"
+  cd /home/config/docker/docker-compose/go
 
-    echo -e "${YELLOW}Введите номер:${NC} "
-    read choice
+  sudo apt-get update -y && sudo apt-get upgrade -y
+  sudo apt-get install wget make tar screen nano build-essential unzip lz4 gcc git jq -y
+
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+
+  export NVM_DIR="/home/config/docker/docker-compose/go/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+  eval "$(cat ~/.bashrc | tail -n +10)"
+
+  nvm install 20
+
+  if ! command -v docker &> /dev/null; then
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    sudo usermod -aG docker $USER
+    rm get-docker.sh
+  else
+    echo "Docker уже установлен. Пропускаем"
+  fi
+
+  if ! command -v docker-compose &> /dev/null; then
+    sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r .tag_name)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+  else
+    echo "Docker-Compose уже установлен. Пропускаем"
+  fi
+
+  curl -L https://foundry.paradigm.xyz | bash
+  eval "$(cat ~/.bashrc | tail -n +10)"
+  foundryup
+
+  cast wallet new --json > wallet_hyperlane.json
+
+  npm install -g @hyperlane-xyz/cli
+
+  docker pull --platform linux/amd64 gcr.io/abacus-labs-dev/hyperlane-agent:agents-v1.0.0
+
+  PRIVATE_KEY=$(jq -r '.[0].private_key' wallet_hyperlane.json)
+  ADDRESS=$(jq -r '.[0].address' wallet_hyperlane.json)
+
+  echo -e "Вот ваши данные от кошелька: "
+  echo -e "Приватный ключ: $PRIVATE_KEY"
+  echo -e "Адресс кошелька: $ADDRESS"
+  echo -e "Пополните данный адрес кошелька в монете ETH в сети Base на несколько долларов"
+}
+
+run_validator() {
+  FILE="wallet_hyperlane.json"
+
+  CHAIN="base"
+  NODE_NAME=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 8)
+  PRIVATE_KEY=$(jq -r '.[0].private_key' wallet_hyperlane.json)
+  
+  while true; do
+    read -p "Введите ссылку на ваш RPC в Base Mainnet: " RPC_CHAIN
+  
+    if [[ $RPC_CHAIN == https://* ]]; then
+      echo "Ссылка корректна!"
+      break
+    else
+      echo "Ошибка: ссылка должна начинаться с https://. Попробуйте еще раз."
+    fi
+  done
+
+  jq --arg node_name "$NODE_NAME" '
+    map(. + {node_name: $node_name})
+  ' "$FILE" > "${FILE}.tmp"
+
+  mv "${FILE}.tmp" "$FILE"
+
+  mkdir -p /home/config/docker/docker-compose/go/hyperlane_db_$CHAIN
+  chmod -R 777 /home/config/docker/docker-compose/go/hyperlane_db_$CHAIN
+
+  docker run -d \
+    -it \
+    --name hyperlane \
+    --mount type=bind,source=/home/config/docker/docker-compose/go/hyperlane_db_$CHAIN,target=/hyperlane_db_$CHAIN \
+    gcr.io/abacus-labs-dev/hyperlane-agent:agents-v1.0.0 \
+    ./validator \
+    --db /hyperlane_db_$CHAIN \
+    --originChainName $CHAIN \
+    --reorgPeriod 1 \
+    --validator.id $NODE_NAME \
+    --checkpointSyncer.type localStorage \
+    --checkpointSyncer.folder $CHAIN \
+    --checkpointSyncer.path /hyperlane_db_$CHAIN/$CHAIN_checkpoints \
+    --validator.key $PRIVATE_KEY \
+    --chains.$CHAIN.signer.key $PRIVATE_KEY \
+    --chains.$CHAIN.customRpcUrls $RPC_CHAIN
+}
+
+check_wallet() {
+  PRIVATE_KEY=$(jq -r '.[0].private_key' wallet_hyperlane.json)
+  ADDRESS=$(jq -r '.[0].address' wallet_hyperlane.json)
+
+  echo -e "Вот ваши данные от кошелька: "
+  echo -e "Приватный ключ: $PRIVATE_KEY"
+  echo -e "Адресс кошелька: $ADDRESS"
+}
+
+check_logs() {
+  docker logs -f hyperlane --tail 300
+}
+
+restart_node() {
+  docker restart hyperlane
+
+  echo 'Ноды была перезагружена.'
+}
+
+delete_node() {
+  read -p 'Если уверены удалить ноду, введите любую букву (CTRL+C чтобы выйти): ' checkjust
+
+  echo 'Начинаю удалять ноду...'
+
+  cd /home/config/docker/docker-compose/go
+
+  docker stop hyperlane
+  docker rm hyperlane
+
+  sudo rm -r hyperlane_db_base/
+  rm wallet_hyperlane.json
+
+  echo "Нода была удалена."
+}
+
+exit_from_script() {
+  exit 0
+}
+
+while true; do
+    channel_logo
+    sleep 2
+    echo -e "\n\nМеню:"
+    echo "1. 🔧 Установить ноду"
+    echo "2. ▶️ Запустить валидатора"
+    echo "3. 👁️ Посмотреть данные от кошелька"
+    echo "4. 📋 Посмотреть логи (выйти CTRL+C)"
+    echo "5. 🔄 Перезагрузить ноду"
+    echo "6. ❌ Удалить ноду"
+    echo -e "7. 🚪 Выйти из скрипта\n"
+    read -p "Выберите пункт меню: " choice
 
     case $choice in
-        1)
-            echo -e "${BLUE}Установка ноды Hyperlane...${NC}"
-
-            # Обновление и установка зависимостей
-            sudo apt update -y
-            sudo apt upgrade -y
-
-            # Проверка наличия Docker
-            if ! command -v docker &> /dev/null; then
-                echo -e "${YELLOW}Docker не установлен. Устанавливаем Docker...${NC}"
-                sudo apt install docker.io -y
-            else
-                echo -e "${GREEN}Docker уже установлен. Пропускаем установку.${NC}"
-            fi
-
-            # Загрузка Docker образа
-            docker pull --platform linux/amd64 gcr.io/abacus-labs-dev/hyperlane-agent:agents-v1.0.0
-
-            # Ввод данных от пользователя
-            echo -e "${YELLOW}Введите имя валидатора:${NC}"
-            read NAME
-            echo -e "${YELLOW}Введите приватный ключ от EVM кошелька начиная с 0x:${NC}"
-            read PRIVATE_KEY
-
-            # Создание директории
-            mkdir -p /home/config/docker/docker-compose/go/hyperlane_db_base && chmod -R 777 /home/config/docker/docker-compose/go/hyperlane_db_base
-
-            # Запуск Docker контейнера
-            docker run -d -it \
-            --name hyperlane \
-            --mount type=bind,source=/home/config/docker/docker-compose/go/hyperlane_db_base,target=/hyperlane_db_base \
-            gcr.io/abacus-labs-dev/hyperlane-agent:agents-v1.0.0 \
-            ./validator \
-            --db /hyperlane_db_base \
-            --originChainName base \
-            --reorgPeriod 1 \
-            --validator.id "$NAME" \
-            --checkpointSyncer.type localStorage \
-            --checkpointSyncer.folder base  \
-            --checkpointSyncer.path /hyperlane_db_base/base_checkpoints \
-            --validator.key "$PRIVATE_KEY" \
-            --chains.base.signer.key "$PRIVATE_KEY" \
-            --chains.base.customRpcUrls https://base.llamarpc.com
-
-            # Заключительное сообщение
-            echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
-            echo -e "${YELLOW}Команда для проверки логов:${NC}"
-            echo "docker logs --tail 100 -f hyperlane"
-            echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
-            echo -e "${GREEN}CRYPTO FORTOCHKA — вся крипта в одном месте!${NC}"
-            echo -e "${CYAN}Наш Telegram https://t.me/cryptoforto${NC}"
-            sleep 2
-            docker logs --tail 100 -f hyperlane
-            ;;
-
-        2)
-            echo -e "${BLUE}Обновление ноды Hyperlane...${NC}"
-            echo -e "${GREEN}Установлена актуальная версия ноды!${NC}"
-            ;;
-
-        3)
-            echo -e "${BLUE}Просмотр логов...${NC}"
-            docker logs --tail 100 -f hyperlane
-            ;;
-
-        4)
-            echo -e "${BLUE}Удаление ноды Hyperlane...${NC}"
-
-            # Остановка и удаление контейнера
-            docker stop hyperlane
-            docker rm hyperlane
-
-            # Удаление папки
-            if [ -d "/home/config/docker/docker-compose/go/hyperlane_db_base" ]; then
-                rm -rf /home/config/docker/docker-compose/go/hyperlane_db_base
-                echo -e "${GREEN}Директория ноды удалена.${NC}"
-            else
-                echo -e "${RED}Директория ноды не найдена.${NC}"
-            fi
-
-            echo -e "${GREEN}Нода Hyperlane успешно удалена!${NC}"
-
-            # Завершающий вывод
-            echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
-            echo -e "${GREEN}CRYPTO FORTOCHKA — вся крипта в одном месте!${NC}"
-            echo -e "${CYAN}Наш Telegram https://t.me/cryptoforto${NC}"
-            sleep 1
-            ;;
-
-        *)
-            echo -e "${RED}Неверный выбор. Пожалуйста, введите номер от 1 до 4.${NC}"
-            ;;
+      1)
+        download_node
+        ;;
+      2)
+        run_validator
+        ;;
+      3)
+        check_wallet
+        ;;
+      4)
+        check_logs
+        ;;
+      5)
+        restart_node
+        ;;
+      6)
+        delete_node
+        ;;
+      7)
+        exit_from_script
+        ;;
+      *)
+        echo "Неверный пункт. Пожалуйста, выберите правильную цифру в меню."
+        ;;
     esac
+  done
